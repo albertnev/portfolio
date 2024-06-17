1ca38ebe27ac024b8f3f8938463adf06e3454050
Albert <8267171+albertnev@users.noreply.github.com>
2024-06-17 17:42:17 +0200

    Fix build runtime errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
+import { inter } from "@/fonts/Inter";
 import "./globals.css";
-
-export const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   description:
