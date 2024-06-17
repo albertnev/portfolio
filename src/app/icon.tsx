70dfbbc254b651656c61db1eb7677eb404d09a7d
Albert <8267171+albertnev@users.noreply.github.com>
2024-06-17 15:36:47 +0200

    Adjust metadata and add custom favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,66 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+  height: 32,
+  width: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+const Icon = () => {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          alignItems: "center",
+          background: "#334155",
+          color: "white",
+          display: "flex",
+          fontSize: "22px",
+          fontWeight: "bold",
+          height: "100%",
+          justifyContent: "center",
+          padding: "4px",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            alignItems: "center",
+            border: "1px solid white",
+            display: "flex",
+            height: "100%",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <span
+            style={{
+              background: "#334155",
+              display: "flex",
+              fontWeight: "bold",
+              left: "8px",
+              paddingRight: "4px",
+              position: "relative",
+            }}
+          >
+            A
+          </span>
+        </div>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  );
+};
+
+export default Icon;
