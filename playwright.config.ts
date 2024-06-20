5a7d185fdc4d4a86ae9e7b64e192723e34f7100f
Albert <8267171+albertnev@users.noreply.github.com>
2024-06-20 20:29:46 +0200

    Set Vercel auth bypass header

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,6 +34,10 @@ export default defineConfig({
   testDir: "./e2e",
   use: {
     baseURL: process.env.BASE_URL ?? "http://localhost:3000",
+    extraHTTPHeaders: {
+      "x-vercel-protection-bypass":
+        process.env.VERCEL_AUTOMATION_BYPASS_SECRET ?? "",
+    },
     trace: "on-first-retry",
   },
   workers: process.env.CI ? 1 : undefined,
