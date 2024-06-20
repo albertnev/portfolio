bc1b490e7614558c89d8b8ba0a89810ae56a874e
Albert <8267171+albertnev@users.noreply.github.com>
2024-06-20 19:07:41 +0200

    Run playwright tests only after Vercel deploy

---
+++ e2e/homePage.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Home Page", () => {
+  test("has the right title and description", async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle("Albert Nevado - Portfolio");
+
+    const metaDescription = page.locator('meta[name="description"]');
+    await expect(metaDescription).toHaveAttribute(
+      "content",
+      "A senior frontend developer's portfolio. If you're hiring, take a look and contact me!",
+    );
+  });
+});
